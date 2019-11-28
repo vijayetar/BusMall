70a8f6d36f7049c4a72db1a59f003cc05c78393a
Vijayeta Rangarajan <vijayetar@gmail.com>
2019-11-28 06:17:59 -0800

    added event listener

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var picArray = [];
 var picOne = document.getElementById('imgOne');
 var picTwo = document.getElementById('imgTwo');
 var picThree = document.getElementById('imgThree');
+var count = 0;
 
 //random number function from MDN
 function randomIndex(max) {
@@ -51,10 +52,11 @@ function generateImages() {
   console.log('index one',indexOne);
 
   picOne.src = picArray[indexOne].src;
+  picOne.alt = picArray[indexOne].alt;
 
   picArray[indexOne].viewed ++;
 
-  console.log('this is the 1st viewed count',picArray[indexOne].viewed);
+  // console.log('this is the 1st viewed count',picArray[indexOne].viewed);
 // generate second index, make sure it is different from the first and then show it
   var indexTwo = randomIndex(picArray.length);
   
@@ -64,9 +66,11 @@ function generateImages() {
   console.log('index two', indexTwo);
 
   picTwo.src = picArray[indexTwo].src;
+  picTwo.alt = picArray[indexTwo].alt;
+
   picArray[indexTwo].viewed ++;
 
-  console.log('this is the 2nd viewed count',picArray[indexTwo].viewed);
+  // console.log('this is the 2nd viewed count',picArray[indexTwo].viewed);
 
   //generate the third image
   var indexThree = randomIndex(picArray.length);
@@ -77,25 +81,64 @@ function generateImages() {
   console.log('index three', indexThree);
 
   picThree.src = picArray[indexThree].src;
+  picThree.alt = picArray[indexThree].alt;
   picArray[indexThree].viewed ++;
   
-  console.log('this is the 3rd viewed count',picArray[indexThree].viewed);
-  console.table(picArray);
+  // console.log('this is the 3rd viewed count',picArray[indexThree].viewed);
+  // console.table(picArray);
+
+  // add ability to click and vote for image
+  
+  if (count === 5) {
+    console.log('countdown completed');
+    endTest();
+    return count;
+  } else {
+    console.log('this is the count',count);
+    picOne.addEventListener('click',handleClick,false);
+    picTwo.addEventListener('click',handleClick,false);
+    picThree.addEventListener('click',handleClick,false);
+  }
+}
+
+
+
+function handleClick(event) {
+  event.preventDefault();
+  console.log('click worked');
+  var vote = event.target.alt;
+  console.log('this is the vote', vote);
+  //loop to check what I clicked and add a value to it
+  for (var i =0; i<picArray.length; i++){
+    if (vote === picArray[i].alt)
+    {
+      picArray[i].clicked ++;
+      count ++;
+      console.log('this is what i clicked',picArray[i].alt);
+    }
+  }
+  // if (count === 5) {
+  //   console.log('countdown completed');
+  //   endTest();
+  //   return;
+  // } else {
+  console.log('this is the count',count);
+  generateImages();
+  // }
 
 }
 
-// fucntion handleClick (){
-//   var vote = event.target.title;
-//   // var clickVote = event.target.index
-//   // var globalPickedArray = e
-//   for (var i =0; i<picArray.length; i++){
-//   if (vote === picArray[i].title)
-//   {
-//     picArray[i].clicked++
-//   }
-//   }
-//   generateImages();
-// }
+function endTest(){
+
+  var result = document.getElementById('results');
+  var countdown = document.getElementById('countdown');
+  countdown.textContent = `You clicked total ${count} times`;
+  result.appendChild(countdown);
+
+}
+
 createOnPageLoad();
 generateImages();
+
+// console.log('here we go again');
 // console.table(picArray);
